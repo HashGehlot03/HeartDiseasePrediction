22a24b113d26e2fd776d28fc3c038474fb93741f
Harish Gehlot <86105660+HashGehlot03@users.noreply.github.com>
2022-01-14 15:51:15 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 mkdir -p ~/.streamlit/
-echo "[general]" > ~/.streamlit/credentials.toml
-echo "email = \" harishgcmc19@svvv.edu.in\"" > ~/.streamlit/credentials.toml
-echo "[server]" > ~/.streamlit/config.toml
-echo "headless = true" >> ~/.streamlit/config.toml
-echo "port = $PORT" >> ~/.streamlit/config.toml
-echo "enableCORS = false" >> ~/.streamlit/config.toml
-
+echo "\
+[server]\n\
+headless = true\n\
+port = $PORT\n\
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
